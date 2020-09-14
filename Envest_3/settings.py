4816619e3fccf6e8a50465a48aa53965e41f72f7
pornchai chaweewat <chaweewat.p@gmail.com>
2020-09-14 15:27:40 +0700

    add django heroku and requirements.txt

--- Envest_3/settings.py
+++ Envest_3/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#activate Django_heroku
+django_heroku.settings(locals())
