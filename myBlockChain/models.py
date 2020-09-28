8475163341028d96d9109cd2d68e009e225da0f8
pornchai chaweewat <chaweewat.p@gmail.com>
2020-09-28 09:29:14 +0700

    [myBlockChain] add view function

--- myBlockChain/models.py
+++ myBlockChain/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class myBlock(models.Model):
+    created = models.DateTimeField(default = datetime.now)
--- myBlockChain/views.py
+++ myBlockChain/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from Crypto.Signature import DSS
+from Crypto.Hash import SHA3_256
+from Crypto.PublicKey import ECC
+from Crypto import Random
+from django.conf import settings
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
