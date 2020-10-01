11e1e0a1636e452b018f8dbad2f43534aa4a2d5e
pornchai chaweewat <34530952+chaweewatp@users.noreply.github.com>
2020-10-01 18:05:36 +0700

    Update settings.py

--- Envest_3/settings.py
+++ Envest_3/settings.py
@@ -32,6 +32,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://google.com',
     'http://hostname.example.com',
     'http://localhost:8000',
+    'http://localhost:3000',
     'http://localhost:3001',
     'http://127.0.0.1:9000',
 ]
